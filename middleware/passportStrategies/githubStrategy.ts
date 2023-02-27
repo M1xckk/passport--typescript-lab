9b3e78837ec73fb918d0e91cc5ce289a624f6450
Brian Xu <mattx0121@gmail.com>
2023-02-26 20:41:50 -0800

    fixed githubstrategy error

--- middleware/passportStrategies/githubStrategy.ts
+++ middleware/passportStrategies/githubStrategy.ts
@@ -1,28 +1,28 @@
 import { Request } from 'express';
 import { Strategy as GitHubStrategy } from 'passport-github2';
 import { PassportStrategy } from '../../interfaces/index';
+// import * as dotenv from 'dotenv';
+require('dotenv').config()
 
 const githubStrategy: GitHubStrategy = new GitHubStrategy(
     {
-        clientID: "",
-        clientSecret: "",
-        callbackURL: "",
-        passReqToCallback: true,
+        clientID: process.env.GITHUB_ID ? process.env.GITHUB_ID : "",
+        clientSecret: process.env.GITHUB_SECRET? process.env.GITHUB_SECRET : "",
+        callbackURL: "http://localhost:8001/auth/login/github/callback",
+        passReqToCallback: true
     },
-    
-    /* done*/
-    async (
-        req: Request, 
-        accessToken: string, 
-        refreshToken: string, 
-        profile: any, 
-        done: (err?: Error | null, profile?:any) => void
-        ) => {}
+
+    /* done */
+   
+    async (req: Request, accessToken: string, refreshToken: string, profile: any, done: (err?: Error | null, profile?: any) => void) => {
+        console.log("PROFILE: ", profile); 
+        done (null, profile);
+
+    },
 );
 
 const passportGitHubStrategy: PassportStrategy = {
     name: 'github',
     strategy: githubStrategy,
 };
-
 export default passportGitHubStrategy;
