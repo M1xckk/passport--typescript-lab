93424fdca8828890da9e1f77eb2b48e90aa53a54
Brian Xu <mattx0121@gmail.com>
2023-02-26 20:45:50 -0800

    fixed localstrategy error

--- middleware/passportStrategies/localStrategy.ts
+++ middleware/passportStrategies/localStrategy.ts
@@ -9,43 +9,39 @@ const localStrategy = new LocalStrategy(
     passwordField: "password",
   },
   (email, password, done) => {
-    try {
-      const user = getUserByEmailIdAndPassword(email, password);
-      if (user) {
-        done(null, user);
-      } else {
-        done(null, false, { message: "Your login details are not valid. Please try again" });
-      }
-    } catch (error) {
-      done(error);
-    }
+    const user = getUserByEmailIdAndPassword(email, password);
+    return user
+      ? done(null, user) 
+      : done(null, false, {
+          message: "Your login details are not valid. Please try again",
+        });
   }
 );
 
 /*
 done
 */
+
 passport.serializeUser(function(
-  user: Express.User,
-  done: (error:any, id:number) => void) {
+  user: Express.User, 
+  done: (error: any,id: number)=>void) {
   done(null, user.id);
 });
 
 /*
 done
 */
-passport.deserializeUser(function(id: number, done:(error:any, user:Express.User | false |null) =>void)  {
-  const user = getUserById(id);
+
+passport.deserializeUser(function (id: number, done: (error: any, user: Express.User | false | null) => void) {
+  let user = getUserById(id);
   if (user) {
     done(null, user);
   } else {
-    done(new Error("User not found"), null);
+    done({ message: "User not found" }, null);
   }
 });
-
 const passportLocalStrategy: PassportStrategy = {
   name: 'local',
   strategy: localStrategy,
 };
-
 export default passportLocalStrategy;
