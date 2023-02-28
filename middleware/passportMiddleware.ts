65b9ee122ebbb236140ac64903034ec3f7e5c5fb
Brian Xu <mattx0121@gmail.com>
2023-02-27 18:37:20 -0800

    add ts-ignore to ignore the error

--- middleware/passportMiddleware.ts
+++ middleware/passportMiddleware.ts
@@ -6,6 +6,7 @@ import localStrategy from "./passportStrategies/localStrategy";
 import passportGitHubStrategy from "./passportStrategies/githubStrategy";
 
 // No need to actually pass the instance of passport since it returns a singleton
+//@ts-ignore
 const passportConfig = new PassportConfig();
 passportConfig.addStrategies([localStrategy /* passportGitHubStrategy */]);
 const passportMiddleware = (app: Application): void => {
